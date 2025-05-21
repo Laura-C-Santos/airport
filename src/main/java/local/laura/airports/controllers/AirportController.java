74e7d384ec3033a5993f13e36607de0c117be359
Laura <lauracrsantos157@gmail.com>
2025-05-21 16:55:41 -0300

    Término da Fase 5 –Update interface AirportRepository – Adicionado assinatura de método Airport findByIataCode(). -Update classe AirportService -Adicionado método findByIataCodeSearch. -Update classe AirportController – Adicionado endpoint iatacode/{iataCode}.

--- src/main/java/local/laura/airports/controllers/AirportController.java
+++ src/main/java/local/laura/airports/controllers/AirportController.java
@@ -57,4 +57,19 @@ public class AirportController {
         }
     
     }
+    
+    @GetMapping("/iatacode/{iataCode}")
+    public ResponseEntity<Airport> findByIataCode(@PathVariable String iataCode){
+        Airport result = airportService.findByIataCode(iataCode);
+        
+        if(result == null){
+            
+            return ResponseEntity.notFound().build();
+        
+        } else{
+        
+            return ResponseEntity.ok(result);
+        }
+    
+    }
 }
